d80dfdee5b6396df1aa87c2bc934181930a03cab
Mayank Agarwal <mayankag658@gmail.com>
2024-07-15 17:20:33 +0530

    Fix Issue

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-var express = require("express"),
-  bodyParser = require("body-parser"),
-  indexPage = require("./routes/index"),
-  assignmentPage = require("./routes/assignment")
-path = require('path'),
-  fs = require('fs'),
-  multer = require('multer'),
-  methodOverride = require("method-override"),
-  app = express(),
-  flash = require("connect-flash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const indexPage = require("./routes/index");
+const assignmentPage = require("./routes/assignment");
+const path = require('path');
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require('express-session');
+
+const app = express();
 const PORT = process.env.PORT || 8000;
-app.set('views', path.join(__dirname + '/views'));
+
+// Setting up view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-// express session
-app.use(require('express-session')({
+// Express session setup
+app.use(session({
   secret: "The snake is flying on pencil nose!!",
   resave: false,
   saveUninitialized: false
